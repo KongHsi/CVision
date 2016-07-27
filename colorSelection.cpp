1c3d5ac7822c4b8dcd2587414f8be200bb8d48f2
Richard Xie <xiezihao@yahoo.ca>
2016-07-27 22:10:10 +0800

    Update colorSelection.cpp

--- colorSelection.cpp
+++ colorSelection.cpp
@@ -11,6 +11,9 @@ using namespace cv;
 int findDis(Vec3b color, Vec3b target);
 void findColor(const Mat &image,Mat &result, Vec3b &target);
 void findEndPoints(const Mat &image, int* result);
+void combineImgs(const Mat &image, Mat &result);
+void erodeImg(Mat &image);
+void dilateImg(Mat &image);
 
 int main (int argc,char** argv){
 
@@ -18,26 +21,27 @@ int main (int argc,char** argv){
 	Mat image = imread("12.jpg");
 	if(!image.data)return -1;
 
+	
 	Mat result(image.rows,image.cols,CV_8UC1);
 	//image.copyTo(result);
 	
-	Vec3b vec=Vec3b(255,0,0);
+	Vec3b vec=Vec3b(250,0,0);
 	
-	findColor(image,result,vec);
-	int midPoint[2];
-	findEndPoints(result, midPoint);
-	result.at<uchar>(midPoint[0],midPoint[1]) = 255;
-	cout<<midPoint[0]<<endl<<midPoint[1]<<endl;
+	//findColor(image,result,vec);
 
-	Mat mix(image.rows,image.cols,CV_8UC3);
-	vector<Mat> planes;
-	split(image,planes);
-	planes[0]+=result;
-	merge(planes,image);
-	
+	//get inverse img of result
+	//threshold(result,result,128,255,THRESH_BINARY_INV);
+
+	//convert color img to B/W
+	cvtColor(image,image, COLOR_BGR2GRAY);
+
+	//combineImgs(image,result);
 
 	namedWindow("image");
-	//threshold(result,result,128,255,THRESH_BINARY_INV);
+	
+	morphologyEx(image,image,MORPH_GRADIENT,Mat());
+	threshold(image,image,40,255,THRESH_BINARY);
+	
 	imshow("image",image);
 	waitKey(0);
 
@@ -58,11 +62,9 @@ void findColor(const Mat &image,Mat &result, Vec3b &target){
 		}
 	}
 }
-
 int findDis(Vec3b color, Vec3b target){
 	return abs(color[0]-target[0])+abs(color[1]-target[1])+abs(color[2]-target[2]);
 }
-
 void findEndPoints(const Mat &image, int* result){
 	int count=0, w=0, h=0;
 	
@@ -75,9 +77,22 @@ void findEndPoints(const Mat &image, int* result){
 			}
 		}
 	}
-
 	result[0] = w/count;
 	result[1] = h/count;
 
 }
-
+void combineImgs(const Mat &image, Mat &result){
+	Mat mix(image.rows,image.cols,CV_8UC3);
+	vector<Mat> planes;
+	split(image,planes);
+	planes[0]+=result*0.3;
+	planes[1]+=result*0.3;
+	planes[2]+=result*0.3;
+	merge(planes,image);
+}
+void erodeImg(Mat &image){
+	erode(image,image,Mat());
+}
+void dilateImg(Mat &image){
+	dilate(image,image,Mat());
+}
