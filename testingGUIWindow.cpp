3262c9bc2f107a53d778de6a8373a0c9438b39b4
Richard Xie <xiezihao@yahoo.ca>
2016-08-06 06:52:40 -0700

    able to add new file

--- testingGUIWindow.cpp
+++ testingGUIWindow.cpp
@@ -4,10 +4,22 @@
 using namespace std;
 using namespace cv;
 
+#include <Windows.h>
+#include <Commdlg.h>
+
+
 void operation(int,void*);
 
+//read new image
 Mat src;
+//image name
+String imgName;
+//two variables adjustable on GUI
 int lightnesslvl =50;
+int button =0;
+//a flag for new image
+bool stateFlag = true;
+
 int main(void){
 
 
@@ -23,15 +35,43 @@ int main(void){
  createTrackbar( "Lightness:\n 0-100", "window",
                  &lightnesslvl, 100,
                  operation);
-
-	operation(0,0);
-	waitKey(0);
+ createTrackbar( "Add Img", "window",
+                 &button, 1,
+                 operation);
+ operation(0,0);
+ waitKey(0);
  
 }
 
 void operation(int,void*){
 	
-	Mat temp = 0.01*lightnesslvl*src;
-	imshow("window",temp);
-
+	//the normal state
+	if(button == 0){
+		Mat temp = 0.01*lightnesslvl*src;
+		imshow("window",temp);
+		stateFlag = true;
+	}
+	//to read a new image
+	if(button == 1 && stateFlag == true){
+		//open a window dialog
+		OPENFILENAMEA ofn;       
+		char szFile[260];       
+		ZeroMemory(&ofn, sizeof(ofn));
+		ofn.lStructSize = sizeof(ofn);
+		ofn.hwndOwner = NULL;
+		ofn.lpstrFile = szFile;
+		ofn.lpstrFile[0] = '\0';
+		ofn.nMaxFile = sizeof(szFile);
+		ofn.lpstrFilter = "*.png";
+		ofn.nFilterIndex = 1;
+		ofn.lpstrFileTitle = NULL;
+		ofn.nMaxFileTitle = 0;
+		ofn.lpstrInitialDir = NULL;
+		ofn.Flags = 0;
+		GetOpenFileNameA(&ofn);
+		imgName = ofn.lpstrFile;
+		Mat img = imread(imgName);
+		imshow("window",img);
+		stateFlag = false;
+	}
 }
